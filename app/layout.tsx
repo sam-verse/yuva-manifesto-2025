7ed0287fac8b123e8e10d0af76f2e4c0b3ae1fa9
same_verse <220801009@rajalakshmi.edu.in>
2025-06-17 18:24:20 +0530

    abraham-manifesto

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="public/favicon.png" sizes="any" />
+        <link rel="icon" href="/favicon.png" sizes="any" />
       </head>
       <body>{children}</body>
     </html>
